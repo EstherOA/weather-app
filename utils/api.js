4e5f163e3e264feca6f66dd5a1a06a90310bd97c
Esther Owusu-Ansah <estherowusuansah19@gmail.com>
2022-03-01 20:42:15 +0000

    Update api.js

--- utils/api.js
+++ utils/api.js
@@ -3,23 +3,24 @@ export const fetchLocationId = async(city) => {
        let response = await fetch('https://www.metaweather.com/api/location/search/?query=' + city);
         let responseJson = await response.json();
         return responseJson[0].woeid;
-        // console.log(responseJson)
-    }catch (error) {
+    } catch (error) {
         console.error(error);
     }
 };
 
 export const fetchWeather = async(woeid) => {
+  const today = new Date();
+  const formattedDate = `/${today.getFullYear()}/${today.getMonth() + 1}/${today.getDate()}`;
     try {
-        let response = await fetch('https://www.metaweather.com/api/location/' + woeid);
+        let response = await fetch('https://www.metaweather.com/api/location/' + woeid + 
+        formattedDate);
         let responseJson = await response.json();
 
-        return {"weather" : responseJson.consolidated_weather[0].weather_state_name,
-                "location" : responseJson.title,
-                "temperature" : responseJson.consolidated_weather[0].the_temp,
-              }  
-
-    }catch (error) {
+        return {"weather" : responseJson[0].weather_state_name,
+                "location" : responseJson[0].title,
+                "temperature" : responseJson[0].max_temp,
+              };  
+    } catch (error) {
         console.log(error);
     }
 };
